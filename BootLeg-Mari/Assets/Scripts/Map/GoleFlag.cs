f066f0f96f1729a33fc9535141a240e6460e0bdd
NiceCreeper3 <124564568+NiceCreeper3@users.noreply.github.com>
2023-06-15 14:47:45 +0200

    Ver21 Win

--- BootLeg-Mari/Assets/Scripts/Map/GoleFlag.cs
+++ BootLeg-Mari/Assets/Scripts/Map/GoleFlag.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class GoleFlag : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject _winUi;
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _winUi.SetActive(true);
     }
 }
